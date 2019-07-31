c047582f0d9863b3de7a3683c9bb8ede24abaecc
Eugene <antipov.jn@gmail.com>
2019-07-31 15:00:59 +0300

    HW29.0

--- src/main/java/test/SaltGeneratorTest.java
+++ src/main/java/test/SaltGeneratorTest.java
@@ -1,27 +1,34 @@
 package test;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import utils.SaltGenerator;
+
 
 import static org.junit.Assert.*;
 
 public class SaltGeneratorTest {
 
+    private static final Logger logger = Logger.getLogger(SaltGeneratorTest.class);
+
+    private static final String TEST_PASS = "TeStPassWORD123";
+
     @Test
     public void getSalt() {
-        assertNotNull("Salt is null", SaltGenerator.getSalt());
-        assertEquals("Salt length is incorrect", 6, SaltGenerator.getSalt().length());
+        assertNotNull( SaltGenerator.getSalt());
+        assertEquals(6, SaltGenerator.getSalt().length());
+        logger.info("TEST DONE!");
     }
 
     @Test
     public void saltPassword() {
-        String password = "admin";
+
         String salt = SaltGenerator.getSalt();
-        String saltPass = SaltGenerator.saltPassword(password, salt);
-        assertEquals("lenth of salt pass is incorrect",
-                salt.length() + password.length(), saltPass.length());
+        String saltPass = SaltGenerator.saltPassword(TEST_PASS, salt);
+        assertEquals(salt.length() + TEST_PASS.length(), saltPass.length());
         assertEquals(salt.substring(0,3),saltPass.substring(0,3));
         assertEquals(salt.substring(salt.length()-3), saltPass.substring(saltPass.length()-3));
-        assertEquals(password, saltPass.substring(3,saltPass.length()-3));
+        assertEquals(TEST_PASS, saltPass.substring(3,saltPass.length()-3));
+        logger.info("TEST DONE!");
     }
 }
